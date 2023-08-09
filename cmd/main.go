9c56375fc88255de8ff62bbb57ecac1570cc18e1
Freddie Molina <freddiejmo@gmail.com>
2023-08-09 16:02:27 -0500

    setup api server

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/freddiemo/logistics-api/api"
 	"github.com/freddiemo/logistics-api/config"
 	"github.com/freddiemo/logistics-api/db"
 )
@@ -12,5 +13,7 @@ func main() {
 	db := db.Init(envs["DB_HOST"], envs["DB_USER"], envs["DB_PASSWORD"], envs["DB_NAME"], envs["DB_PORT"])
 
 	fmt.Println(db)
+	api.Init(envs)
+
 	fmt.Println("logistics api")
 }
